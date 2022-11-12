958f1867d13d8477ce52476c4ac5105e18c907ec
JOEX <oyinjoe23@gmail.com>
2022-11-12 19:07:46 +0100

    Created order components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import FAQS from '../../components/FAQS'
 import MainHeader from '../../components/MainHeader'
+import Programs from '../../components/Programs'
+import Values from '../../components/Values'
 import "./Home.css"
 
 const Home = () => {
   return (
     <>
     <MainHeader/>
-
+    <Programs/>
+    <Values/>
+    <FAQS/>
     </>
   )
 }
