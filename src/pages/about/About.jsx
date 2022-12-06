3a5b26992f2d26cdaf1cfb9db687d0e2a12b3b0e
JOEX <oyinjoe23@gmail.com>
2022-12-06 01:43:27 +0100

    feature: Contact page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,8 @@
 import Header from "../../components/Header";
 import HeaderImage from "../../images/header_bg_1.jpg";
 import StoryImage from "../../images/about1.jpg";
+import VisionImage from "../../images/about2.jpg";
+import MissionImage from "../../images/about3.jpg";
 
 import "./About.css";
 
@@ -13,13 +15,76 @@ const About = () => {
       </Header>
 
       <section className="about__story">
-        <div className="container about__container">
-          <div className="about__section-img">
-            <img src={StoryImage} alt="Our Story"/>
+        <div className="container about__story-container">
+          <div className="about__section-image">
+            <img src={StoryImage} alt="Our Story img" />
           </div>
-
+          <div className="about__section-content">
+            <h1>Our Story</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam
+              deserunt modi eveniet ipsam, sed est, quibusdam necessitatibus
+              voluptas eligendi aperiam molestiae, laborum blanditiis vel ut
+              architecto eos quos?
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              hic, autem molestias, beatae corrupti reprehenderit voluptates
+              magnam facere fuga maxime non id quos ducimus iste!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quo
+              enim in impedit hic distinctio tempora?
+              </p>
+          </div>
         </div>
+      </section>
+      <section className="about__vision">
+        <div className="container about__vision-container">
+          <div className="about__section-content">
+            <h1>Our Vision</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam
+              deserunt modi eveniet ipsam, sed est, quibusdam necessitatibus
+              voluptas eligendi aperiam molestiae, laborum blanditiis vel ut
+              architecto eos quos?
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              hic, autem molestias, beatae corrupti reprehenderit voluptates
+              magnam facere fuga maxime non id quos ducimus iste!
+            </p>
+          </div>
+          <div className="about__section-image">
+            <img src={VisionImage} alt="Our Vision img" />
+          </div>
+        </div>
+      </section>
 
+      <section className="about__mission">
+        <div className="container about__mission-container">
+          <div className="about__section-image">
+            <img src={MissionImage} alt="Our Mission img" />
+          </div>
+          <div className="about__section-content">
+            <h1>Our Mission</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam
+              deserunt modi eveniet ipsam, sed est, quibusdam necessitatibus
+              voluptas eligendi aperiam molestiae, laborum blanditiis vel ut
+              architecto eos quos?
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              hic, autem molestias, beatae corrupti reprehenderit voluptates
+              magnam facere fuga maxime non id quos ducimus iste!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quo
+              enim in impedit hic distinctio tempora?
+              </p>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_2.jpg"
+import  {MdEmail } from 'react-icons/md'
+import  { BsMessenger } from 'react-icons/bs'
+import  { IoLogoWhatsapp } from 'react-icons/io'
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <Header title='Get In Touch'  image={HeaderImage}>
+    Consectetur adipisicing elit. Non ipsa dolorem, rem consequatur eum
+        omnis ex, fuga temporibus qui nesciunt odio aliquam.
+    </Header>
+
+    <section className='contact'>
+      <div className='container contact__container'>
+       <div className='contact__wrapper'>
+        <a href="mailto:oyinjoe23@gmail.com" target="_blank" rel='noreferrer noopener'><MdEmail/></a>
+        <a href="https://m.me/joseph.oyinloye.351" target="_blank" rel='noreferrer noopener'><BsMessenger/></a>
+        <a href="https://wa.me/+2348118276455" target="_blank" rel='noreferrer noopener'><IoLogoWhatsapp/></a>
+       </div>
+
+      </div>
+
+    </section>
+    </>
   )
 }
 
